75c3e27ef69621856f951595c061f68234b8d46e
programaker <programaker@gmail.com>
2019-02-27 00:17:53 +0100

    Now it compiles with Either

--- src/main/scala/joguin/player/Interact.scala
+++ src/main/scala/joguin/player/Interact.scala
@@ -5,22 +5,12 @@ import scalaz.Free._
 sealed trait InteractF[A]
 case class WriteMessage(message: String) extends InteractF[Unit]
 case object ReadAnswer extends InteractF[String]
-
-/*trait ParseAnswerF[A]
-trait ValidateAnswerF[A]*/
+case class ParseAnswer[B](value: String) extends InteractF[Either[String,B]]
+case class ValidateAnswer[B](parsed: B) extends InteractF[Either[String,B]]
 
 object Interact {
-  /*def writeMessage(message: String): Interact[Unit] = liftF(WriteMessage(message))
-  def readAnswer: Interact[String] = liftF(ReadAnswer)
-  def parseAnswer[A](answer: String, pa: ParseAnswerE[A]): ParseAnswer[A] = liftF(pa)
-  def validateAnswer[A](parsedAnswer: A, va: ValidateAnswerE[A]): ValidateAnswer[A] = liftF(va)
-
-  def ask[A](message: String, pa: ParseAnswerE[A], va: ValidateAnswerE[A]): Interact[A] = for {
-    _ <- writeMessage(message)
-    answer <- readAnswer
-    parsedAnswer <- parseAnswer(answer, pa)
-    validatedAnswer <- validateAnswer(parsedAnswer, va)
-  } yield {
-    validatedAnswer
-  }*/
+  def write(message: String): Interact[Unit] = liftF(WriteMessage(message))
+  def read: Interact[String] = liftF(ReadAnswer)
+  def parse[B](value: String): Interact[Either[String,B]] = liftF[InteractF, Either[String,B]](ParseAnswer(value))
+  def validate[B](parsed: B): Interact[Either[String,B]] = liftF[InteractF, Either[String,B]](ValidateAnswer(parsed))
 }
--- src/main/scala/joguin/player/package.scala
+++ src/main/scala/joguin/player/package.scala
@@ -4,10 +4,4 @@ import scalaz.Free
 
 package object player {
   type Interact[A] = Free[InteractF,A]
-
-  /*type ParseAnswerE[T] = ParseAnswerF[Either[String,T]]
-  type ParseAnswer[T] = Free[ParseAnswerE, T]
-
-  type ValidateAnswerE[T] = ValidateAnswerF[Either[String,T]]
-  type ValidateAnswer[T] = Free[ValidateAnswerE, T]*/
 }
