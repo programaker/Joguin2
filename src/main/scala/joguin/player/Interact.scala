ec3237b39eec48718176a4e8e735cb8464576e93
programaker <programaker@gmail.com>
2019-02-27 00:21:05 +0100

    Better names

--- src/main/scala/joguin/player/Interact.scala
+++ src/main/scala/joguin/player/Interact.scala
@@ -9,8 +9,8 @@ case class ParseAnswer[B](value: String) extends InteractF[Either[String,B]]
 case class ValidateAnswer[B](parsed: B) extends InteractF[Either[String,B]]
 
 object Interact {
-  def write(message: String): Interact[Unit] = liftF(WriteMessage(message))
-  def read: Interact[String] = liftF(ReadAnswer)
-  def parse[B](value: String): Interact[Either[String,B]] = liftF[InteractF, Either[String,B]](ParseAnswer(value))
-  def validate[B](parsed: B): Interact[Either[String,B]] = liftF[InteractF, Either[String,B]](ValidateAnswer(parsed))
+  def writeMessage(message: String): Interact[Unit] = liftF(WriteMessage(message))
+  def readAnswer: Interact[String] = liftF(ReadAnswer)
+  def parseAnswer[B](value: String): Interact[Either[String,B]] = liftF[InteractF, Either[String,B]](ParseAnswer(value))
+  def validateAnswer[B](parsed: B): Interact[Either[String,B]] = liftF[InteractF, Either[String,B]](ValidateAnswer(parsed))
 }
