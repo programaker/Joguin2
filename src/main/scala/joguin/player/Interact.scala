75875ac627632f4a9d9752d6a06a857d01c142fa
programaker <programaker@gmail.com>
2019-02-28 22:55:15 +0100

    Add missing validateAnswer and replace recursion with another Op

--- src/main/scala/joguin/player/Interact.scala
+++ src/main/scala/joguin/player/Interact.scala
@@ -9,6 +9,7 @@ case class WriteMessage(message: String) extends InteractOp[Unit]
 case object ReadAnswer extends InteractOp[String]
 case class ParseAnswer[T](value: String) extends InteractOp[Either[String,T]]
 case class ValidateAnswer[T](parsed: T) extends InteractOp[Either[String,T]]
+case class Retry[T]() extends InteractOp[T]
 
 class Interact[F[_]](implicit I: InjectK[InteractOp,F]) {
   def writeMessage(message: String): Free[F,Unit] =
@@ -22,11 +23,23 @@ class Interact[F[_]](implicit I: InjectK[InteractOp,F]) {
 
   def validateAnswer[T](parsed: T): Free[F,Either[String,T]] =
     inject[InteractOp,F](ValidateAnswer(parsed))
+
+  def retry[T]: Free[F,T] =
+    inject[InteractOp,F](Retry())
 }
 
 object Interact {
   implicit def create[F[_]](implicit I: InjectK[InteractOp,F]): Interact[F] = new Interact
 
+  /** To reuse the following flow in all game steps:
+    *
+    * Ask something from player
+    * -> Read response
+    * -> Parse response
+    * -> Validate parsed response
+    * -> Tell player about the error
+    * -> Retry until the player give a valid answer
+    * */
   def ask[F[_],T](message: String)(implicit I: Interact[F]): Free[F,T] = {
     import I._
 
@@ -36,9 +49,14 @@ object Interact {
       parsedAnswer <- parseAnswer[T](answer)
     } yield parsedAnswer
 
-    parsedAnswer.flatMap {
-      case Right(b) => pure(b)
-      case Left(error) => writeMessage(error).flatMap(_ => ask(message))
+    val validatedAnswer = parsedAnswer.flatMap {
+      case Right(t) => validateAnswer[T](t)
+      case left @ Left(_) => pure[F,Either[String,T]](left)
+    }
+
+    validatedAnswer.flatMap {
+      case Right(t) => pure[F,T](t)
+      case Left(error) => writeMessage(error).flatMap(_ => retry[T])
     }
   }
 }
