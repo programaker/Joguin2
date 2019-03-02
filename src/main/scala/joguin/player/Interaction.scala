98d4bcd755ec43d50aecceead4d70e2f3fb0692c
programaker <programaker@gmail.com>
2019-03-02 12:46:39 +0100

    Better name and remotion of an evolutionary remnant

--- src/main/scala/joguin/player/Interaction.scala
+++ src/main/scala/joguin/player/Interaction.scala
@@ -4,28 +4,28 @@ import cats.{Functor, InjectK}
 import cats.free.Free
 import cats.free.Free._
 
-sealed trait InteractOp[A]
-case class WriteMessage(message: String) extends InteractOp[Unit]
-case object ReadAnswer extends InteractOp[String]
-case class ParseAnswer[T](value: String) extends InteractOp[Either[String,T]]
-case class ValidateAnswer[T](parsed: T) extends InteractOp[Either[String,T]]
+sealed trait InteractionOp[A]
+case class WriteMessage(message: String) extends InteractionOp[Unit]
+case object ReadAnswer extends InteractionOp[String]
+case class ParseAnswer[T](value: String) extends InteractionOp[Either[String,T]]
+case class ValidateAnswer[T](parsed: T) extends InteractionOp[Either[String,T]]
 
-class Interact[F[_]](implicit I: InjectK[InteractOp,F]) {
+class Interaction[F[_]](implicit I: InjectK[InteractionOp,F]) {
   def writeMessage(message: String): Free[F,Unit] =
-    inject[InteractOp,F](WriteMessage(message))
+    inject[InteractionOp,F](WriteMessage(message))
 
   def readAnswer: Free[F,String] =
-    inject[InteractOp,F](ReadAnswer)
+    inject[InteractionOp,F](ReadAnswer)
 
   def parseAnswer[T](value: String): Free[F,Either[String,T]] =
-    inject[InteractOp,F](ParseAnswer(value))
+    inject[InteractionOp,F](ParseAnswer(value))
 
   def validateAnswer[T](parsed: T): Free[F,Either[String,T]] =
-    inject[InteractOp,F](ValidateAnswer(parsed))
+    inject[InteractionOp,F](ValidateAnswer(parsed))
 }
 
-object Interact {
-  implicit def create[F[_]](implicit I: InjectK[InteractOp,F]): Interact[F] = new Interact
+object Interaction {
+  implicit def create[F[_]](implicit I: InjectK[InteractionOp,F]): Interaction[F] = new Interaction
 
   /** To reuse the following flow in all game steps:
     *
@@ -36,7 +36,7 @@ object Interact {
     * -> Tell player about the error
     * -> Retry until the player give a valid answer
     * */
-  def ask[F[_],T](message: String)(implicit I: Interact[F], F: Functor[F]): Free[F,T] = {
+  def ask[F[_],T](message: String)(implicit I: Interaction[F]): Free[F,T] = {
     import I._
 
     val parsedAnswer = for {
