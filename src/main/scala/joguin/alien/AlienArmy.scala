3f7805b1d75aa9b97f9230a30299e33f5a3c7918
programaker <programaker@gmail.com>
2019-03-02 13:18:08 +0100

    AlienArmy is just an utility to create Invasions, it does not need to be a Free monad

--- src/main/scala/joguin/alien/AlienArmy.scala
+++ src/main/scala/joguin/alien/AlienArmy.scala
@@ -1,25 +1,14 @@
 package joguin.alien
 
-import cats.InjectK
 import cats.free.Free
-import cats.free.Free._
-import joguin.earth.city.City
-import joguin.alien.Invasion.InProgress
 import eu.timepit.refined.auto._
+import joguin.alien.Invasion.InProgress
+import joguin.earth.city.City
 
-trait AlienArmyOp[A]
-case class Attack(city: City) extends AlienArmyOp[Invasion]
-
-class AlienArmy[F[_]](implicit I: InjectK[AlienArmyOp,F]) {
-  def attack(city: City): Free[F,Invasion] = inject[AlienArmyOp,F](Attack(city))
-}
-
+/** Attacks a city installing a Terraform Device in it, resulting an invasion.
+  * The Terraform Devices gain a random defense power to make things more interesting */
 object AlienArmy {
-  implicit def create[F[_]](implicit I: InjectK[AlienArmyOp,F]): AlienArmy[F] = new AlienArmy[F]
-
-  /** Attacks a city installing a Terraform Device in it, resulting an invasion.
-    * The Terraform Devices gain a random defense power to make things more interesting */
-  def attack[F[_]](city: City)(P: PowerGenerator[F]): Free[F,Invasion] = {
+  def attack[F[_]](city: City)(implicit P: PowerGenerator[F]): Free[F,Invasion] = {
     import P._
 
     generatePower(min = 1000, max = 20000)
