aa6636669616748fe426465d6d6352484d0e6a79
programaker <programaker@gmail.com>
2019-04-21 00:08:24 +0200

    Fix regex bug that would require 2 digits always

--- src/main/scala/joguin/game/step/explore/package.scala
+++ src/main/scala/joguin/game/step/explore/package.scala
@@ -6,5 +6,5 @@ import eu.timepit.refined.generic.Equal
 import eu.timepit.refined.string.MatchesRegex
 
 package object explore {
-  type ExploreOptionR = MatchesRegex[W.`"""^[1-9][0-9]+$"""`.T] Or Equal[W.`"q"`.T]
+  type ExploreOptionR = MatchesRegex[W.`"""^[1-9][0-9]*$"""`.T] Or Equal[W.`"q"`.T]
 }
