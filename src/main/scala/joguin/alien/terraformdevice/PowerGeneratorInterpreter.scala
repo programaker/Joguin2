79ddb1de47bd9a4d065c0dd61ebb36bdaae6dd73
programaker <programaker@gmail.com>
2019-04-26 00:22:36 +0200

    Effect-agnostic Power Generator Interpreter

---
+++ src/main/scala/joguin/alien/terraformdevice/PowerGeneratorInterpreter.scala
@@ -0,0 +1,29 @@
+package joguin.alien.terraformdevice
+
+import java.util.concurrent.ThreadLocalRandom
+
+import cats.~>
+import eu.timepit.refined.auto._
+import eu.timepit.refined.refineV
+import joguin.alien.{Power, PowerR}
+
+/** PowerGeneratorF root interpreter to any F that produces random numbers between min and max */
+final class PowerGeneratorInterpreter[F[_]](wrap: (=>Power) => F[Power]) extends (PowerGeneratorF ~> F) {
+  override def apply[A](fa: PowerGeneratorF[A]): F[A] = fa match {
+    case GeneratePower(min, max) => randomPowerBetween(min, max)
+  }
+
+  private def randomPowerBetween(min: Power, max: Power): F[Power] =
+    wrap(impureRandomPower(min, max))
+
+  private def impureRandomPower(min: Power, max: Power): Power =
+    refineV[PowerR](ThreadLocalRandom.current.nextInt(min, max + 1)) match {
+      case Right(generatedPower) => generatedPower
+      case Left(_) => min //in case of error, falls back to min value and life goes on...
+    }
+}
+
+object PowerGeneratorInterpreter {
+  def apply[F[_]](wrap: (=>Power) => F[Power]): PowerGeneratorInterpreter[F] =
+    new PowerGeneratorInterpreter(wrap)
+}
