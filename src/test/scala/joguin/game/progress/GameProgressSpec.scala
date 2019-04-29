d7e3e92df36262d29de7c494321067ba6d0e4797
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-04-29 16:30:13 +0200

    Testing GameProgress 2

--- src/test/scala/joguin/game/progress/GameProgressSpec.scala
+++ src/test/scala/joguin/game/progress/GameProgressSpec.scala
@@ -2,21 +2,45 @@ package joguin.game.progress
 
 import joguin.alien.Invasion
 import joguin.earth.maincharacter.MainCharacter
-import joguin.testutil.PropertyBasedSpec
 import joguin.testutil.Generators._
+import joguin.testutil.PropertyBasedSpec
+import org.scalatest.OptionValues._
 
 class GameProgressSpec extends PropertyBasedSpec {
-  property("GameProgress should start with a 0-experience MainCharacter and no defeated Invasions") {
+  property("should start with a 0-experience MainCharacter and no defeated Invasions") {
     forAll { (mainCharacter: MainCharacter, invasions: List[Invasion]) =>
       val gp = GameProgress.start(mainCharacter, invasions)
 
-      val invasionNumber = invasions.size
+      val invasionCount = invasions.size
       gp.mainCharacterExperience.value shouldBe 0
       gp.defeatedInvasions.value shouldBe 0
       gp.allInvasionsDefeated shouldBe false
       gp.defeatedInvasionsTrack shouldBe empty
-      gp.invasionCount.value shouldBe invasionNumber
-      gp.indexedInvasions should have size invasionNumber
+      gp.invasionCount.value shouldBe invasionCount
+      gp.indexedInvasions should have size invasionCount
+    }
+  }
+
+  property("should return an Invasion given a valid index") {
+    forAll { (mainCharacter: MainCharacter, invasions: List[Invasion], index: Index) =>
+      val gp = GameProgress.start(mainCharacter, invasions)
+      val maxIndex = invasions.size
+
+      whenever(index.value <= maxIndex) {
+        val invasionAtIndex = invasions(index.value - 1)
+        gp.invasionByIndex(index).value shouldBe invasionAtIndex
+      }
+    }
+  }
+
+  property("should return None given an invalid index") {
+    forAll { (mainCharacter: MainCharacter, invasions: List[Invasion], index: Index) =>
+      val gp = GameProgress.start(mainCharacter, invasions)
+      val maxIndex = invasions.size
+
+      whenever(index.value > maxIndex) {
+        gp.invasionByIndex(index) shouldBe empty
+      }
     }
   }
 }
--- src/test/scala/joguin/testutil/Generators.scala
+++ src/test/scala/joguin/testutil/Generators.scala
@@ -8,16 +8,18 @@ import joguin.{Name, NameR}
 import joguin.earth.{Country, CountryR}
 import joguin.earth.city.City
 import joguin.earth.maincharacter.{Age, AgeR, Female, Gender, MainCharacter, Male, Other}
+import joguin.game.progress.{Index, IndexR}
 import org.scalacheck.{Arbitrary, Gen}
 
 object Generators {
   implicit val arbCity: Arbitrary[City] = Arbitrary(genCity)
   implicit val arbMainCharacter: Arbitrary[MainCharacter] = Arbitrary(genMainCharacter)
   implicit val arbInvasionList: Arbitrary[List[Invasion]] = Arbitrary(genInvasionList)
+  implicit val arbIndex: Arbitrary[Index] = Arbitrary(genIndex)
 
 
   def genInvasionList: Gen[List[Invasion]] = {
-    Gen.containerOfN[List, Invasion](6, genInvasion)
+    Gen.containerOfN[List, Invasion](10, genInvasion)
   }
 
   def genInvasion: Gen[Invasion] =
@@ -72,5 +74,13 @@ object Generators {
     Gen.choose(min = AlienArmy.minPower.value, max = AlienArmy.maxPower.value)
       .map(refineV[PowerR](_))
       .map(_.getOrElse(elsePower))
+  }
+
+  def genIndex: Gen[Index] = {
+    val elseIndex: Index = 1
+
+    Gen.choose(min = 1, max = 20)
+      .map(refineV[IndexR](_))
+      .map(_.getOrElse(elseIndex))
   }
 }
