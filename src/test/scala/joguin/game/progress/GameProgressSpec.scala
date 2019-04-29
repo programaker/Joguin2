80c5f84a2464c00035b2a31118bca3d2634c6f52
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-04-29 18:32:29 +0200

    Testing GameProgress 5

--- src/test/scala/joguin/game/progress/GameProgressSpec.scala
+++ src/test/scala/joguin/game/progress/GameProgressSpec.scala
@@ -1,9 +1,12 @@
 package joguin.game.progress
 
+import eu.timepit.refined._
+import eu.timepit.refined.auto._
 import joguin.alien.Invasion
 import joguin.earth.maincharacter.MainCharacter
 import joguin.testutil.Generators._
 import joguin.testutil.PropertyBasedSpec
+import org.scalatest.Inspectors
 import org.scalatest.OptionValues._
 
 @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
@@ -11,14 +14,19 @@ class GameProgressSpec extends PropertyBasedSpec {
   property("should start with a 0-experience MainCharacter and no defeated Invasions") {
     forAll { (mainCharacter: MainCharacter, invasions: List[Invasion]) =>
       val gp = GameProgress.start(mainCharacter, invasions)
-
       val invasionCount = invasions.size
+
+      val shouldNotHappen: Index = 1
+      val indexes = (1 to invasionCount).map(refineV[IndexR](_).getOrElse(shouldNotHappen))
+
       gp.mainCharacterExperience.value shouldBe 0
-      gp.defeatedInvasions.value shouldBe 0
       gp.allInvasionsDefeated shouldBe false
-      gp.defeatedInvasionsTrack shouldBe empty
-      gp.invasionCount.value shouldBe invasionCount
-      gp.indexedInvasions should have size invasionCount
+
+      Inspectors.forAll(indexes) { idx =>
+        val invasionAtIndex = invasions(idx.value - 1)
+        gp.invasionByIndex(idx).value shouldBe invasionAtIndex
+        gp.isInvasionDefeated(idx) shouldBe false
+      }
     }
   }
 
@@ -45,17 +53,14 @@ class GameProgressSpec extends PropertyBasedSpec {
     }
   }
 
-  property("should mark the Invasion at a given index as defeated") {
+  property("should track the defeating of the Invasion at a given index") {
     forAll { (mainCharacter: MainCharacter, invasions: List[Invasion], index: Index) =>
       val gp = GameProgress.start(mainCharacter, invasions)
       val maxIndex = invasions.size
 
       whenever(index.value <= maxIndex) {
         val gp1 = gp.defeatInvasion(index)
-
         gp1.isInvasionDefeated(index) shouldBe true
-        gp1.defeatedInvasions.value shouldBe 1
-        gp1.defeatedInvasionsTrack should contain(index)
         gp1.allInvasionsDefeated shouldBe false
       }
     }
@@ -70,6 +75,23 @@ class GameProgressSpec extends PropertyBasedSpec {
       }
 
       gp1.mainCharacterExperience.value shouldBe (start.mainCharacterExperience.value + (n * xp.value))
+    }
+  }
+
+  property("should track the defeating of all Invasions") {
+    forAll { (mainCharacter: MainCharacter, invasions: List[Invasion]) =>
+      val start = GameProgress.start(mainCharacter, invasions)
+      val invasionCount = invasions.size
+
+      val shouldNotHappen: Index = 1
+      val indexes = (1 to invasionCount).map(refineV[IndexR](_).getOrElse(shouldNotHappen))
+
+      val gp1 = indexes.foldLeft(start) { (gp, idx) =>
+        gp.defeatInvasion(idx)
+      }
+
+      gp1.allInvasionsDefeated shouldBe true
+      Inspectors.forAll(indexes){ idx => gp1.isInvasionDefeated(idx) shouldBe true }
     }
   }
 }
