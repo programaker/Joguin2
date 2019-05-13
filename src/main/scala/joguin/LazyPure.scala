d2c9a45755cc7c359bef51dfa8157d10b6e5fd3e
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-05-13 12:08:47 +0200

    Generalization of MessagesF Interpreter to any F [2]

--- src/main/scala/joguin/LazyPure.scala
+++ src/main/scala/joguin/LazyPure.scala
@@ -4,17 +4,17 @@ import cats.Id
 import cats.effect.IO
 
 trait LazyPure[F[_]] {
-  def lazyPure[A](body: =>A): F[A]
+  def lazyPure[A](a: =>A): F[A]
 }
 
 object LazyPure {
   def apply[F[_]](implicit instance: LazyPure[F]): LazyPure[F] = instance
 
   implicit val IOLazyPure: LazyPure[IO] = new LazyPure[IO] {
-    override def lazyPure[A](body: =>A): IO[A] = IO(body)
+    override def lazyPure[A](a: =>A): IO[A] = IO(a)
   }
 
   implicit val IdLazyPure: LazyPure[Id] = new LazyPure[Id] {
-    override def lazyPure[A](body: =>A): Id[A] = body:Id[A]
+    override def lazyPure[A](a: =>A): Id[A] = a:Id[A]
   }
 }
