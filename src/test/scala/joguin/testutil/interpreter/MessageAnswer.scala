3439baf217588776b2a3f6d73f21c709177f2f2b
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-05-13 18:20:31 +0200

    State monad alias to track interactions in game step tests

--- src/test/scala/joguin/testutil/interpreter/MessageAnswer.scala
+++ src/test/scala/joguin/testutil/interpreter/MessageAnswer.scala
@@ -1,7 +1,10 @@
 package joguin.testutil.interpreter
 
+import cats.data.State
+
 final case class MessageAnswer(message: String, answer: String)
 
 object MessageAnswer {
   type Track = Map[Int, MessageAnswer]
+  type TrackState[A] = State[Track, A]
 }
