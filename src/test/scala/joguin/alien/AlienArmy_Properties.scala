8ada4a31be052c7acaa8480a80abebaa2db44abb
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-05-13 17:48:00 +0200

    It seems like aggregate the interpreters in a single place won't be that useful

--- src/test/scala/joguin/alien/AlienArmy_Properties.scala
+++ src/test/scala/joguin/alien/AlienArmy_Properties.scala
@@ -1,13 +1,15 @@
 package joguin.alien
 
+import cats.Id
 import joguin.alien.terraformdevice.PowerGeneratorF
+import joguin.alien.terraformdevice.PowerGeneratorInterpreter
 import joguin.alien.terraformdevice.PowerGeneratorOps._
 import joguin.earth.city.City
-import joguin.testutil.interpreter.Interpreters._
 import joguin.testutil.PropertyBasedSpec
 
 @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
 final class AlienArmy_Properties extends PropertyBasedSpec {
+  private val powerGeneratorInterpreter = PowerGeneratorInterpreter[Id]
 
   property("invading a City produces an Invasion with a TerraformDevice, whose power is between 1000 and 20000") {
     import joguin.testutil.generator.Generators.city
