173fe8f1c08622962d281c8b710583a922b5f1c5
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-05-16 17:51:20 +0200

    [WIP] Possibility of many answers per question in tests

--- src/test/scala/joguin/game/step/createcharacter/CreateCharacterStep_Properties.scala
+++ src/test/scala/joguin/game/step/createcharacter/CreateCharacterStep_Properties.scala
@@ -7,9 +7,16 @@ import joguin.earth.maincharacter.Gender
 import joguin.earth.maincharacter.MainCharacter
 import joguin.game.step.Explore
 import joguin.testutil.PropertyBasedSpec
+import joguin.testutil.generator.AgeGenerators
+import joguin.testutil.generator.Generators
+import joguin.testutil.generator.Tag
+import joguin.testutil.generator.Tag.T1
+import joguin.testutil.generator.Tag.T2
 import joguin.testutil.interpreter.CreateCharacterStepInterpreter
 import joguin.testutil.interpreter.CreateCharacterStepInterpreter.CreateCharacterStepF
 import joguin.testutil.interpreter.WriteMessageTrack
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
 import org.scalatest.Inside._
 import org.scalatest.OptionValues._
 
@@ -41,15 +48,15 @@ final class CreateCharacterStep_Properties extends PropertyBasedSpec {
       age: Age
     ) =>
 
-      val expectedAnswers: Map[String, String] = Map(
-        askName -> name,
-        askGender -> gender.code,
-        askAge -> age.toString
+      val answers: Map[String, List[String]] = Map(
+        askName -> List(name),
+        askGender -> List(gender.code),
+        askAge -> List(age.toString)
       )
 
       val track = CreateCharacterStep[CreateCharacterStepF].start
-        .foldMap(CreateCharacterStepInterpreter.build(expectedAnswers))
-        .runS(WriteMessageTrack.empty)
+        .foldMap(CreateCharacterStepInterpreter.build)
+        .runS(WriteMessageTrack.build(answers))
 
       val actualMessages = track.map(_.indexedMessages).value
       actualMessages.get(0).value shouldBe askToCreateCharacter
@@ -60,7 +67,7 @@ final class CreateCharacterStep_Properties extends PropertyBasedSpec {
     }
   }
 
-  property("goes to the Explore step after the creation of the MainCharacter") {
+  property("goes to the Explore step, passing the created MainCharacter") {
     import joguin.testutil.generator.Generators.age
     import joguin.testutil.generator.Generators.gender
     import joguin.testutil.generator.Generators.name
@@ -73,20 +80,45 @@ final class CreateCharacterStep_Properties extends PropertyBasedSpec {
 
       val expectedMainChar = MainCharacter(name, gender, age)
 
-      val expectedAnswers: Map[String, String] = Map(
-        askName -> name,
-        askGender -> gender.code,
-        askAge -> age.toString
+      val answers: Map[String, List[String]] = Map(
+        askName -> List(name),
+        askGender -> List(gender.code),
+        askAge -> List(age.toString)
       )
 
       val nextStep = CreateCharacterStep[CreateCharacterStepF].start
-        .foldMap(CreateCharacterStepInterpreter.build(expectedAnswers))
-        .runA(WriteMessageTrack.empty)
+        .foldMap(CreateCharacterStepInterpreter.build)
+        .runA(WriteMessageTrack.build(answers))
 
       inside(nextStep.value) { case Explore(progress) =>
         progress.mainCharacter shouldBe expectedMainChar
       }
     }
   }
+
+  /*
+  property("repeats a question to the player until receives a valid answer") {
+    import Tag._
+    import Tag.implicits._
+    import joguin.testutil.generator.Generators.age
+    import joguin.testutil.generator.Generators.gender
+    import joguin.testutil.generator.Generators.name
+    import joguin.testutil.generator.Generators.invalidName
+
+    val i1: Arbitrary[Tag[T1, Int]] = arbTag(Generators.genSmallInt)
+    val i2: Arbitrary[Tag[T2, Int]] = arbTag(AgeGenerators.genInvalidAge)
+
+    forAll { (
+      name: Name,
+      gender: Gender,
+      age: Age,
+      n: Tag[T1, Int],
+      invalidName: String,
+      invalidAge: Tag[T2, Int]
+    ) =>
+
+
+    }
+  }*/
 
 }
