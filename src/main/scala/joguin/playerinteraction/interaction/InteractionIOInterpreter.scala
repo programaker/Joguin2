b488093893c116e704ea3794cdc5171bb126f7d1
programaker <programaker@gmail.com>
2019-07-30 08:19:10 +0200

    #21 [WIP] Generalizing InteractionInterpreter

--- src/main/scala/joguin/playerinteraction/interaction/InteractionIOInterpreter.scala
+++ src/main/scala/joguin/playerinteraction/interaction/InteractionIOInterpreter.scala
@@ -6,12 +6,12 @@ import cats.~>
 import scala.io.StdIn.readLine
 
 /** InteractionF root interpreter to IO that interacts with the player through the console */
-object InteractionIOInterpreter extends (InteractionF ~> IO) {
-  override def apply[A](fa: InteractionF[A]): IO[A] = fa match {
+object InteractionIOInterpreter[F[_] : Lazy] extends (InteractionF ~> F) {
+  override def apply[A](fa: InteractionF[A]): F[A] = fa match {
     case WriteMessage(message) => write(message)
     case ReadAnswer            => read()
   }
 
-  private def write(message: String): IO[Unit] = IO(print(message))
-  private def read(): IO[String] = IO(readLine())
+  private def write(message: String): F[Unit] = Lazy[F].lift(print(message))
+  private def read(): F[String] = Lazy[F].lift(readLine())
 }
