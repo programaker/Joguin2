75594e37ae1796462381b8117b76b305c354bf37
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-07-30 11:11:28 +0200

    #21 Rename Lazy instances

--- src/main/scala/joguin/Lazy.scala
+++ src/main/scala/joguin/Lazy.scala
@@ -11,11 +11,11 @@ trait Lazy[F[_]] {
 object Lazy {
   def apply[F[_]](implicit instance: Lazy[F]): Lazy[F] = instance
 
-  implicit val ioLazyEff: Lazy[IO] = new Lazy[IO] {
+  implicit val ioLazy: Lazy[IO] = new Lazy[IO] {
     override def lift[A](a: => A): IO[A] = IO(a)
   }
 
-  implicit val idLazyEff: Lazy[Id] = new Lazy[Id] {
+  implicit val idLazy: Lazy[Id] = new Lazy[Id] {
     override def lift[A](a: => A): Id[A] = Monad[Id].pure(a)
   }
 }
