8342ca9d9a9ee5612935b0626ccd731560ebcbed
programaker <programaker@gmail.com>
2019-10-03 17:19:11 +0200

    Use abstract class for typeclass

--- src/main/scala/joguin/Lazy.scala
+++ src/main/scala/joguin/Lazy.scala
@@ -8,7 +8,7 @@ import zio.Task
  *
  * Useful to store impure code inside a proper functional data type without
  * coupling with any specific IO implementation */
-trait Lazy[F[_]] {
+abstract class Lazy[F[_]] {
   def lift[A](a: => A): F[A]
 }
 
