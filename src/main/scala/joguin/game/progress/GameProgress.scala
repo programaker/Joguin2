8212566c93b8f959f2350e3b274b56b7b042440d
programaker <programaker@gmail.com>
2019-10-07 23:21:47 +0200

    Remove closed-task TODO comment

--- src/main/scala/joguin/game/progress/GameProgress.scala
+++ src/main/scala/joguin/game/progress/GameProgress.scala
@@ -3,7 +3,6 @@ package joguin.game.progress
 import joguin.alien.Invasion
 import joguin.earth.maincharacter.MainCharacter
 
-/* TODO => Big GameProgress Refactoring! https://github.com/programaker/Joguin2/issues/4 */
 final case class GameProgress(
   mainCharacter: MainCharacter,
   invasions: Vector[Invasion] //TODO => Use some Non-empty data structure or refinement
