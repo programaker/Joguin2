c9232af7324c0fd4f42a9067306312242b214b67
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-10-14 13:45:51 +0200

    generators.experience

--- src/test/scala/joguin/testutil/generators/experience/package.scala
+++ src/test/scala/joguin/testutil/generators/experience/package.scala
@@ -6,12 +6,12 @@ import joguin.earth.maincharacter.Experience
 import joguin.earth.maincharacter.ExperienceR
 import org.scalacheck.Gen
 
-object ExperienceGenerators {
+package object experience {
   def genExperience: Gen[Experience] =
     Gen
       .choose(min = 0, max = 20000)
       .map(refineV[ExperienceR](_))
-      .map(_.getOrElse(0: Experience))
+      .map(_.getOrElse(0))
 
   def genInvalidExperience: Gen[Int] =
     Gen.choose(Int.MinValue, -1)
