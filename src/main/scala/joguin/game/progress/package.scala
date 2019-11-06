1516ce201ff01f494d0fea0f4d6660d457d9c6bc
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2019-11-06 16:15:49 +0100

    #15 Using the Index optic

--- src/main/scala/joguin/game/progress/package.scala
+++ src/main/scala/joguin/game/progress/package.scala
@@ -1,6 +1,5 @@
 package joguin.game
 
-import cats.implicits._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
 import eu.timepit.refined.numeric.NonNegative
@@ -15,7 +14,7 @@ import joguin.earth.maincharacter.ExperienceR
 import monocle.Lens
 import monocle.macros.GenLens
 import monocle.function.all.index
-import monocle.function.Index._
+import monocle.syntax.ApplyOptional
 import monocle.syntax.apply._
 
 package object progress {
@@ -25,17 +24,23 @@ package object progress {
   type IndexR = Positive
   type Index = Int Refined IndexR
 
+  type InvasionsApplyOptional = ApplyOptional[IdxSeq[Invasion], IdxSeq[Invasion], Invasion, Invasion]
+  type DefeatedApplyOptional = ApplyOptional[IdxSeq[Invasion], IdxSeq[Invasion], Boolean, Boolean]
+
   val ExperienceField: Lens[GameProgress, Experience] = GenLens[GameProgress](_.mainCharacter.experience)
   val CharacterNameField: Lens[GameProgress, Name] = GenLens[GameProgress](_.mainCharacter.name)
   val InvasionsField: Lens[GameProgress, IdxSeq[Invasion]] = GenLens[GameProgress](_.invasions)
 
-  def InvasionAtIndex(gp: GameProgress, idx: Index) =
-    gp.invasions.applyOptional(index(idx - 1))
-
-  def invasionByIndex(gp: GameProgress, index: Index): Option[Invasion] =
+  def InvasionFieldAtIndex(gp: GameProgress, idx: Index): InvasionsApplyOptional =
     //1-based index, to match the invasion list as the player sees it
     //and also the player's input when select an invasion to fight
-    gp.invasions.unapply(index - 1)
+    gp.invasions.applyOptional(index(idx - 1))
+
+  def DefeatedFieldAtIndex(gp: GameProgress, idx: Index): DefeatedApplyOptional =
+    InvasionFieldAtIndex(gp, idx).composeLens(DefeatedField)
+
+  def invasionByIndex(gp: GameProgress, index: Index): Option[Invasion] =
+    InvasionFieldAtIndex(gp, index).getOption
 
   def isInvasionDefeated(gp: GameProgress, index: Index): Boolean =
     invasionByIndex(gp, index).exists(_.defeated)
@@ -49,10 +54,5 @@ package object progress {
     gp.invasions.forall(_.defeated)
 
   def defeatInvasion(gp: GameProgress, index: Index): GameProgress =
-    invasionByIndex(gp, index)
-      .map { invasion =>
-        val defeatedInvasion = invasion.copy(defeated = true)
-        gp.copy(invasions = gp.invasions.updated(index - 1, defeatedInvasion))
-      }
-      .getOrElse(gp)
+    InvasionsField.set(DefeatedFieldAtIndex(gp, index).modify(_ => true))(gp)
 }
